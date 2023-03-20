1be4496beea6dd846aff557eb114b95bc47ecba1
mferrin <ferrin.michael.84@gmail.com>
2023-03-20 10:11:38 -0400

    Clean up db.js

--- db.js
+++ db.js
@@ -7,14 +7,21 @@ const pool = mariadb.createPool({
   database: "MealPlanner"
 });
 
-module.exports={
-  getConnection: function(){
-    return new Promise(function(resolve,reject){
-      pool.getConnection().then(function(connection){
-        resolve(connection);
-      }).catch(function(error){
-        reject(error);
-      });
-    });
+pool.getConnection((err, connection) => {
+  if(err){
+    if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+      console.error('Database connection lost');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR'){
+      console.error('Database has too many connections');
+    };
+    if (err.code === 'ECONNREFUSED'){
+      console.error('Database connection was refused');
+    }
   }
-}
+  if(connection) connection.release();
+
+  return;
+});
+
+module.exports = pool;
